e95ffded95c0d7fc26a161d13a0dd178393a5aa9
Angela Oryza <admin@Angelas-MacBook-Air.local>
2022-09-14 15:11:13 +0700

    Gambar 3 Titik

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [ 
+        0.5, 0.5, //A : Kanan atas
+        0.0, 0.0,//B : Bawah Tengah
+        -0.5, 0.5 //C : Kiri Atas
+    ]; //buat array untuk segitiga, tapi ini masih di cpu
+
+    var buffer = gl.createBuffer(); 
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer); // ini define alamat gpu
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    //udah tersambung ke gpu (udah dipindah ke alam GPU)
+
     //Vertex Shaders
     var vertexShaderCode = 
     `
+        attribute vec2 aPosition;
         void main() {
-            float x = 0.0;
-            float y = 0.0;
+            float x = aPosition.x;
+            float y = aPosition.y;
             gl_PointSize = 10.0;
             gl_Position = vec4(x, y, 0.0, 1.0);
             
@@ -39,11 +51,20 @@ function main() {
     gl.linkProgram(shaderProgram); 
     gl.useProgram(shaderProgram);
 
+    //Mengajari GPU bagaimana caranya mengoleksi
+    // nilai posisi dari ARRAY_BUFFER
+    //untuk setiap verteks yang sedang diproses
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    //                      index   dimensi              warna
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0,  0.0,    5.0,    1.0);
     //            Red   Green   Blue    Alpha(Opacity)
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0,1); 
+    gl.drawArrays(gl.POINTS, 0, 3); 
     //          jenis      data     count
 }
 
